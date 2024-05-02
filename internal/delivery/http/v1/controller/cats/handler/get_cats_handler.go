3b462d15bb94237d3d0a6c9e9fb93eee0724f682
abdmmar <opowae909@gmail.com>
2024-05-02 19:59:50 +0700

    fix: make validation permissive

--- internal/delivery/http/v1/controller/cats/handler/get_cats_handler.go
+++ internal/delivery/http/v1/controller/cats/handler/get_cats_handler.go
@@ -5,7 +5,7 @@ import (
 	"cats-social/internal/delivery/http/v1/response"
 	"cats-social/pkg/lumen"
 	"cats-social/pkg/util"
-	"errors"
+	"strconv"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
@@ -19,19 +19,12 @@ func (ch catHandler) GetCats(c *fiber.Ctx) error {
 		err  error
 	)
 	queryParams := c.Queries()
-
-	err = c.QueryParser(&req)
-	if err != nil {
-		return lumen.FromError(lumen.NewError(lumen.ErrBadRequest, err)).SendResponse(c)
-	}
-
 	validator := validator.New()
-	err = validator.Struct(req)
-	if err != nil {
-		return lumen.FromError(lumen.NewError(lumen.ErrBadRequest, err)).SendResponse(c)
-	}
-
 	ctx := c.Context()
+
+	req = request.GetCats{
+		Search: queryParams["search"],
+	}
 
 	shouldFilters := request.ShouldFilters{
 		ID:         queryParams["id"] != "",
@@ -45,19 +38,74 @@ func (ch catHandler) GetCats(c *fiber.Ctx) error {
 		Search:     queryParams["search"] != "",
 	}
 
-	if shouldFilters.AgeInMonth {
-		num := util.ParseAgeInMonth(req.AgeInMonth)
-
-		if num == -1 {
-			return lumen.FromError(lumen.NewError(lumen.ErrBadRequest, errors.New("invalid age in month value"))).SendResponse(c)
+	if shouldFilters.ID {
+		err := validator.Var(queryParams["id"], "uuid")
+		if err != nil {
+			shouldFilters.ID = false
+		} else {
+			req.ID = queryParams["id"]
 		}
 	}
 
-	if queryParams["limit"] == "" {
+	if shouldFilters.Race {
+		err := validator.Var(queryParams["race"], "oneof=Persian 'Maine Coon' Ragdoll Bengal Sphynx 'British Shorthair' Abyssinian 'Scottish Fold' Birman")
+		if err != nil {
+			shouldFilters.Race = false
+		} else {
+			req.Race = queryParams["race"]
+		}
+	}
+
+	if shouldFilters.Sex {
+		err := validator.Var(queryParams["sex"], "oneof=male female")
+		if err != nil {
+			shouldFilters.Sex = false
+		} else {
+			req.Sex = queryParams["sex"]
+		}
+	}
+
+	if shouldFilters.HasMatched {
+		err := validator.Var(queryParams["hasMatched"], "boolean")
+		if err != nil {
+			shouldFilters.HasMatched = false
+		} else {
+			val, _ := strconv.ParseBool(queryParams["hasMatched"])
+			req.HasMatched = val
+		}
+	}
+
+	if shouldFilters.Owned {
+		err := validator.Var(queryParams["owned"], "boolean")
+		if err != nil {
+			shouldFilters.Owned = false
+		} else {
+			val, _ := strconv.ParseBool(queryParams["owned"])
+			req.Owned = val
+		}
+	}
+
+	if shouldFilters.AgeInMonth {
+		num := util.ParseAgeInMonth(queryParams["ageInMonth"])
+
+		if num == -1 {
+			shouldFilters.AgeInMonth = false
+		} else {
+			req.AgeInMonth = queryParams["ageInMonth"]
+		}
+	}
+
+	if shouldFilters.Limit {
+		val, _ := strconv.ParseInt(queryParams["limit"], 10, 32)
+		req.Limit = int32(val)
+	} else {
 		req.Limit = 5
 	}
 
-	if queryParams["offset"] == "" {
+	if shouldFilters.Offset {
+		val, _ := strconv.ParseInt(queryParams["offset"], 10, 32)
+		req.Offset = int32(val)
+	} else {
 		req.Offset = 0
 	}
 
