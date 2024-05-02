f46cc892fbf41da582b6551eb96ddd1759333ff8
Zaki <achmadikazum91@gmail.com>
2024-05-02 23:33:22 +0700

    handling error for not found id

--- internal/delivery/http/v1/controller/cats/handler/delete_cat_handler.go
+++ internal/delivery/http/v1/controller/cats/handler/delete_cat_handler.go
@@ -3,7 +3,8 @@ package catHandler
 import (
 	"cats-social/internal/delivery/http/v1/response"
 	"github.com/gofiber/fiber/v2"
-	"fmt"
+	"github.com/lib/pq"
+	// "fmt"
 )
 
 func (ch catHandler) DeleteCat(c *fiber.Ctx) error {
@@ -16,12 +17,21 @@ func (ch catHandler) DeleteCat(c *fiber.Ctx) error {
 
 	ctx := c.Context()
 
-	// Delete cat using service
 	resp, err = ch.catService.DeleteCat(ctx, catID)
 	if err != nil {
-		fmt.Println(err)
-		return fiber.ErrBadRequest
+		// Check if the error is due to invalid UUID input syntax
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code.Name() == "invalid_text_representation" {
+			// Return 404 Not Found with custom message if invalid UUID
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "id tidak ditemukan",
+			})
+		}
 	}
 
-	return c.JSON(resp)
+	return c.JSON(response.Common{
+		Message: "success",
+		Data:    resp,
+	})
 }
+
+
